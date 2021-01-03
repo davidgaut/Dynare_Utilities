73e3fb9d2b132058da891dc208804a8316d022b1
David Gauthier <david_gauthier@orange.fr>
2021-01-03 22:57:36 +0000

    update freq estimation

--- FrequencyEstimation/frequency_estimation_init.m
+++ FrequencyEstimation/frequency_estimation_init.m
@@ -1,5 +1,7 @@
 function [Iy,varobs_idx] = frequency_estimation_init(dataset_info,M_,oo_,options_)
 
+fprintf('Initialize frequency domain estimation, frequency is %d, %d.\n',options_.estim_frequency)
+
 x     = dataset_info.rawdata';
 T     = length(x);
 t     = repmat((1 : T),size(x,1),1);
